da68ae41bafb7b543188e72dca8bb92940f02ffb
Craig Walls <craig@habuma.com>
2019-02-20 23:39:05 -0700

    Remove unnecessary configuration

--- jwt-resource-server/src/main/java/habuma/OAuth2ResourceServerConfig.java
+++ jwt-resource-server/src/main/java/habuma/OAuth2ResourceServerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
